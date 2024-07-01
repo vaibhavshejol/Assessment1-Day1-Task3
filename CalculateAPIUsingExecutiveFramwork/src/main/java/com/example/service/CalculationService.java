3934dc86edd79d75b857b0d2ec0ad82494fe66a0
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-01 23:21:00 +0530

    calculation service created

--- CalculateAPIUsingExecutiveFramwork/src/main/java/com/example/service/CalculationService.java
+++ CalculateAPIUsingExecutiveFramwork/src/main/java/com/example/service/CalculationService.java
@@ -1,15 +1,29 @@
 package com.example.service;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CalculationService {
 
+    private static final Logger log = LoggerFactory.getLogger(CalculationService.class);
+    private final ExecutorService executor = Executors.newFixedThreadPool(8);
+
     public void performCalculations(int number) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'performCalculations'");
+        executor.submit(() -> calculateSquare(number));
+        executor.submit(() -> calculateCube(number));
+        executor.submit(() -> checkEven(number));
+        executor.submit(() -> checkPrime(number));
+        executor.submit(() -> checkArmstrong(number));
+        executor.submit(() -> checkPalindrome(number));
+        executor.submit(() -> calculateFactorial(number));
+        executor.submit(() -> calculateReverse(number));
     }
 
     public void shutdownExecutor() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'shutdownExecutor'");
+        executor.shutdown();
     }
     
 }
