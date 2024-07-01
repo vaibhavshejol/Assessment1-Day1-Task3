1523ef4b81140c6243da542a8c4ddbcdf1ed6207
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-01 22:40:42 +0530

    Project folder structure created

---
+++ CalculateAPIUsingExecutiveFramwork/src/main/java/com/example/CalculateApiUsingExecutiveFramworkApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateApiUsingExecutiveFramworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateApiUsingExecutiveFramworkApplication.class, args);
+		System.out.println("Application started successfully.....");
+	}
+
+}
